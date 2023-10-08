3f8e6194f8ec5cbf5fac1ed0fec766ad6513264b
Oxamyt <dentez.danil@gmail.com>
2023-10-08 21:55:23 +0300

    create-one-round-game

--- script.js
+++ script.js
@@ -3,4 +3,39 @@ let answers = ['rock', 'paper', 'scissors']
 return answers[Math.floor(Math.random()*answers.length)]
 }
 
-console.log(getComputerChoice())
+const playerSelection = prompt('Choose a tool').toLowerCase();
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+   if (playerSelection == 'rock' && computerSelection == 'paper'){
+      alert('you loose')
+   }
+   else if (playerSelection == 'rock' && computerSelection == 'scissors'){
+      alert('you win')
+   }
+   else if (playerSelection == 'paper' && computerSelection == 'scissors'){
+      alert('you loose')
+   }
+   else if (playerSelection == 'paper' && computerSelection == 'rock'){
+      alert('you win')
+   }
+   else if (playerSelection == 'scissors' && computerSelection == 'rock'){
+      alert('you loose')
+   }
+   else if (playerSelection == 'scissors' && computerSelection == 'paper'){
+      alert('you win')
+   }
+   else if (playerSelection != ['rock', 'paper', 'scissors'] ){
+      alert ('wrong type')
+   }
+   else {
+      alert('not win, not loose')
+   }
+
+}
+
+function game(){
+   
+}
+
+playRound(playerSelection, computerSelection);
