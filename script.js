6751402fe087771a6ecfc5ea895760de7699561e
Oxamyt <dentez.danil@gmail.com>
2023-10-09 23:38:22 +0300

    fix-rounds-bug,create-score

--- script.js
+++ script.js
@@ -1,39 +1,90 @@
+// computer answer
 function getComputerChoice (){
 let answers = ['rock', 'paper', 'scissors']
 return answers[Math.floor(Math.random()*answers.length)]
 }
 
-const playerSelection = prompt('Choose a tool').toLowerCase().trim();
-const computerSelection = getComputerChoice();
+// variables
+let userWin = 'You Win!';
+let computerWin = 'Computer Wins!';
+let draw = 'Draw!';
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
 
+// game function
 function playRound(playerSelection, computerSelection){
    if (playerSelection == 'rock' && computerSelection == 'paper'){
-      alert('you loose')
+      alert ('You Lost!');
+      return computerWin;
    }
    else if (playerSelection == 'rock' && computerSelection == 'scissors'){
-      alert('you win')
+      alert ('You Win!');
+      return userWin;
    }
    else if (playerSelection == 'paper' && computerSelection == 'scissors'){
-      alert('you loose')
+      alert ('You Lost!');
+      return computerWin;
    }
    else if (playerSelection == 'paper' && computerSelection == 'rock'){
-      alert('you win')
+      alert ('You Win!');
+      return userWin;
    }
    else if (playerSelection == 'scissors' && computerSelection == 'rock'){
-      alert('you loose')
+      alert ('You Lost!');
+      return computerWin;
    }
    else if (playerSelection == 'scissors' && computerSelection == 'paper'){
-      alert('you win')
+      alert ('You Win!');
+      return userWin;
    }
    else if (playerSelection !== 'rock' && playerSelection !==  'paper' && playerSelection !== 'scissors'){
-      alert ('wrong type')
+      alert ('wrong type');
    }
    else {
-      alert('not win, not loose')
+      alert ('draw')
+      return draw;
    }
 
 }
 
+function game(){
+const playerSelection = prompt('Choose a tool').toLowerCase().trim();
+const computerSelection = getComputerChoice();
+const result = playRound(playerSelection, computerSelection);
 
+if (result === userWin){
+   playerScore++;
+}
+else if(result === computerWin){
+   computerScore++;
+}
+else if(result === draw){
+   draws++;
+}
+console.log(`Player Score ${playerScore}`);
+console.log(`Computer Score ${computerScore}`);
+console.log(`Draws ${draws}`);
 
-playRound(playerSelection, computerSelection)
+if (playerScore >= 5 || computerScore >= 5 || draws >=5){
+   gameScore(playerScore, computerScore, draws)
+}
+else {
+   game()
+}
+
+} 
+
+function gameScore(playerScore, computerScore, draws){
+   if (playerScore >= 5){
+      console.log('You have won the game!')
+   }
+   else if (computerScore >= 5){
+      console.log('Computer have won the game!')
+   }
+   else if (draws >= 5){
+      console.log('GREAT DRAW')
+   }
+}
+
+game();
